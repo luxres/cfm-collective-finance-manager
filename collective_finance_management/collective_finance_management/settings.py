c4bb699d145462e6cb51a37aef531ec028b7181b
lux-maker <socpopcap@proton.me>
2026-07-09 13:55:41 +0200

    included CSRF_TRUSTED_ORIGINS and SECURE_PROXY_SSL_HEADER to run the app behind a reverse rpoxy

--- collective_finance_management/collective_finance_management/settings.py
+++ collective_finance_management/collective_finance_management/settings.py
@@ -18,6 +18,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -29,6 +30,10 @@ DEBUG = os.environ.get("DEBUG", default=0)
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
+if os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS"):
+    CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS")
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
 
